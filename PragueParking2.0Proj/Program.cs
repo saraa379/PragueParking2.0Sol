ccda9ea31cc441dd6b739af8144adad756890bc5
saraa379 <saraa379@yahoo.com>
2021-01-14 12:58:16 +0100

    Add project files.

---
+++ PragueParking2.0Proj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PragueParking2._0Proj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
